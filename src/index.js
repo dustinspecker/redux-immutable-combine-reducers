d552b32befbf0563afe7b96d8d0a78a83745a114
Dustin Specker <DustinSpecker@DustinSpecker.com>
2016-11-12 14:21:01 -0600

    chore(test): fix eslint issues

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 'use strict'
 import isPlainObj from 'is-plain-obj'
-import {Map} from 'immutable'
+import Immutable from 'immutable'
 
 /**
  * Returns a reducer function that always returns an Immutable.Map
@@ -15,20 +15,20 @@ module.exports = function (reducers) {
       }
     })
 
-    return (state = Map({}), action) =>
+    return (state = Immutable.Map({}), action) =>
       Object.keys(reducers).reduce((nextstate, key) =>
         nextstate.set(key, reducers[key](state.get(key), action))
       , state)
   }
 
-  if (Map.isMap(reducers)) {
+  if (Immutable.Map.isMap(reducers)) {
     reducers.keySeq().forEach(key => {
       if (typeof reducers.get(key) !== 'function') {
         throw new Error('Expected an Immutable.Map with all entries being functions')
       }
     })
 
-    return (state = Map({}), action) =>
+    return (state = Immutable.Map({}), action) =>
       reducers.keySeq().reduce((nextstate, key) =>
         nextstate.set(key, reducers.get(key)(state.get(key), action))
       , state)
